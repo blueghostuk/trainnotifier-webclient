blueghostuk/trainnotifier-webclient

commit 0e6f6a38c9548b4b81e7f6d0a2d2e9432ecba918

remove old search. remove old date format code, use moment.js. remove hard-coded :82 port

WebClient/Scripts/ViewModels.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="knockout-2.2.0.js" />
-/// <reference path="common.js" />
+﻿/// <reference path="moment.js" />
+/// <reference path="knockout-2.2.0.js" />
 
 function LocationViewModel() {
     this.locationStanox = ko.observable();
@@ -10,41 +10,6 @@ function LocationViewModel() {
     this.stationLocation = ko.observable();
     this.Lat = ko.observable();
     this.Lon = ko.observable();
-}
-
-function WttSearchResult() {
-    var self = this;
-
-    self.TrainId = ko.observable();
-    self.TrainUid = ko.observable();
-    self.Headcode = ko.observable();
-    self.WttId = ko.observable();
-    self.From = ko.observable();
-    self.Depart = ko.observable();
-    self.To = ko.observable();
-    self.Arrive = ko.observableArray();
-
-    self.addStop = function (stop) {
-        self.Stops.push(stop);
-    };
-
-    self.clearStops = function () {
-        self.Stops.removeAll();
-    }
-}
-
-function WttSearchResults() {
-    var self = this;
-
-    self.Trains = ko.observableArray();
-
-    self.addTrain = function (stop) {
-        self.Trains.push(stop);
-    };
-
-    self.clearTrains = function () {
-        self.Trains.removeAll();
-    }
 }
 
 function ScheduleSearchResults() {
@@ -154,8 +119,7 @@ function getTimes(stopEl) {
         Delay: 0
     };
     if (stopEl.ActualTimeStamp && stopEl.ActualTimeStamp.length > 0) {
-        var actualTime = new Date(stopEl.ActualTimeStamp);
-        result.ActualTimeStamp = formatTimeString(actualTime);
+        result.ActualTimeStamp = moment(stopEl.ActualTimeStamp).format("HH:mm:ss");
     } else {
         result.ActualTimeStamp = "";
         setTimes = false;
@@ -163,10 +127,9 @@ function getTimes(stopEl) {
 
     if (stopEl.PlannedTime && stopEl.PlannedTime.length > 0) {
         var plannedTime = new Date(stopEl.PlannedTime);
-        result.PlannedTimeStamp = formatTimeString(plannedTime);
+        result.PlannedTimeStamp = moment(stopEl.PlannedTime).format("HH:mm:ss");
     } else if (stopEl.ActualTimeStamp && stopEl.ActualTimeStamp.length > 0) {
-        var plannedTime = new Date(stopEl.ActualTimeStamp);
-        result.PlannedTimeStamp = formatTimeString(actualTime);
+        result.PlannedTimeStamp = moment(stopEl.ActualTimeStamp).format("HH:mm:ss");
     } else {
         result.PlannedTimeStamp = "";
         setTimes = false;

WebClient/App_Start/BundleConfig.cs
@@ -22,7 +22,6 @@ namespace TrainNotifier.WebClient
                 "~/Scripts/moment*"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                "~/Scripts/common.js",
                 "~/Scripts/websockets.js",
                 "~/Scripts/ajax-lookups.js",
                 "~/Scripts/mapping.js",
@@ -33,9 +32,6 @@ namespace TrainNotifier.WebClient
 
             bundles.Add(new ScriptBundle("~/bundles/train").Include(
                 "~/Scripts/train.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/search").Include(
-                "~/Scripts/search.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/search-schedule").Include(
                 "~/Scripts/search-schedule.js"));

WebClient/Scripts/ajax-lookups.js
@@ -4,7 +4,7 @@ function fetchStanox(stanox) {
     if (!stanox || stanox.length == 0)
         return;
 
-    $.getJSON("http://" + server + ":82/Stanox/" + stanox, function (data) {
+    $.getJSON("http://" + server + "/Stanox/" + stanox, function (data) {
         var html = "";
         if (data.StationName) {
             html = data.StationName;
@@ -25,7 +25,7 @@ function loadLocation(stanox, callback) {
     if (!stanox || stanox.length == 0)
         return;
 
-    $.getJSON("http://" + server + ":82/Stanox/" + stanox, function (data) {
+    $.getJSON("http://" + server + "/Stanox/" + stanox, function (data) {
         if (!callback)
             callback = loadLocationCallback;
         callback(data);
@@ -39,7 +39,7 @@ function filter(location) {
         return;
     }
 
-    $.getJSON("http://" + server + ":82/Station/", { id: location }, function (data) {
+    $.getJSON("http://" + server + "/Station/", { id: location }, function (data) {
         currentFilter = data.Name;
         $("#table-trains tbody tr").hide();
         $("#table-trains tbody tr." + data.Name).show();
@@ -47,7 +47,7 @@ function filter(location) {
 }
 
 function preLoadStations(callback) {
-    $.getJSON("http://" + server + ":82/Station/", null, function (results) {
+    $.getJSON("http://" + server + "/Station/", null, function (results) {
         if (!results || results.length == 0)
             return;
 

WebClient/Scripts/common.js
@@ -1,16 +1,0 @@
-﻿function formatDateString(d) {
-    function pad(n) { return n < 10 ? '0' + n : n }
-    return pad(d.getUTCDate()) + '/'
-        + pad(d.getUTCMonth() + 1) + '/'
-        + d.getUTCFullYear() + ' '
-        + pad(d.getUTCHours()) + ':'
-        + pad(d.getUTCMinutes()) + ':'
-        + pad(d.getUTCSeconds());
-}
-
-function formatTimeString(d) {
-    function pad(n) { return n < 10 ? '0' + n : n }
-    return pad(d.getUTCHours()) + ':'
-        + pad(d.getUTCMinutes()) + ':'
-        + pad(d.getUTCSeconds());
-}

WebClient/Scripts/search-schedule.js
@@ -39,7 +39,7 @@ function parseCommand() {
 function getOrigin(args, convertFromCrs) {
     if (convertFromCrs) {
         document.location.hash = "listorigin-crs:" + args;
-        $.getJSON("http://" + server + ":82/Stanox/?GetByCrs&crsCode=" + args, function (data) {
+        $.getJSON("http://" + server + "/Stanox/?GetByCrs&crsCode=" + args, function (data) {
             getOriginByStanox(data.Name);
         });
     } else {
@@ -51,7 +51,7 @@ function getOrigin(args, convertFromCrs) {
 function getStation(args, convertFromCrs) {
     if (convertFromCrs) {
         document.location.hash = "liststation-crs:" + args;
-        $.getJSON("http://" + server + ":82/Stanox/?GetByCrs&crsCode=" + args, function (data) {
+        $.getJSON("http://" + server + "/Stanox/?GetByCrs&crsCode=" + args, function (data) {
             getCallingAtStanox(data.Name);
         });
     } else {
@@ -82,7 +82,7 @@ function getOriginByStanox(stanox, date) {
         listStation(currentStanox);
     }
 
-    $.getJSON("http://" + server + ":82/TrainMovement/StartingAtStation/" + currentStanox +
+    $.getJSON("http://" + server + "/TrainMovement/StartingAtStation/" + currentStanox +
         "?startDate=" + now.format(dateFormatQuery) +
         "&endDate=" + new moment(now).add('days', 1).format(dateFormatQuery),
         function (data) {
@@ -128,7 +128,7 @@ function getCallingAtStanox(stanox, date) {
         listStation(currentStanox);
     }
 
-    $.getJSON("http://" + server + ":82/TrainMovement/CallingAtStation/" + currentStanox +
+    $.getJSON("http://" + server + "/TrainMovement/CallingAtStation/" + currentStanox +
         "?startDate=" + now.format(dateFormatQuery) +
         "&endDate=" + new moment(now).add('days', 1).format(dateFormatQuery),
         function (data) {
@@ -190,7 +190,7 @@ function listStation(stanox) {
     $('html, body').animate({
         scrollTop: $("#locationDetails").offset().top
     }, 1000);
-    $.getJSON("http://" + server + ":82/Stanox/" + stanox, function (data) {
+    $.getJSON("http://" + server + "/Stanox/" + stanox, function (data) {
         currentLocation.locationStanox(data.Name);
         currentLocation.locationTiploc(data.Tiploc);
         currentLocation.locationDescription(data.Description);

WebClient/Scripts/search.js
@@ -1,193 +1,0 @@
-﻿var currentLocation = new LocationViewModel();
-
-function setCommand(command) {
-    $("#filter-command").val(command);
-}
-
-function parseCommand() {
-    var cmdString = $("#filter-command").val();
-    var idx = cmdString.indexOf(":");
-    if (idx == -1)
-        return;
-
-    var cmd = cmdString.substring(0, idx);
-    var args = cmdString.substring(idx + 1);
-
-    switch (cmd) {
-        case 'getservice':
-            getService(args);
-            break;
-        case 'listorigin':
-            getOrigin(args);
-            break;
-        case 'listorigin-crs':
-            getOrigin(args, true);
-            break;
-        case 'liststation':
-            getStation(args);
-            break;
-        case 'liststation-crs':
-            getStation(args, true);
-            break;
-    }
-}
-
-function createWttSearchResult(trainMovement) {
-    var result = new WttSearchResult();
-    result.TrainId(trainMovement.Id);
-    result.TrainUid(trainMovement.TrainUid);
-    result.Headcode(trainMovement.HeadCode);
-    result.WttId(trainMovement.WorkingTTId);
-    result.From(trainMovement.SchedOriginStanox);
-    result.Depart(trainMovement.SchedOriginDeparture);
-    result.To('');
-    result.Arrive('');
-    return result;
-}
-
-function showWttResults(trainMovements) {
-    currentWttResult.clearTrains();
-    if (trainMovements.length > 0) {
-        $("#no-results-row").hide();
-        for (i in trainMovements) {
-            var result = createWttSearchResult(trainMovements[i]);
-
-            currentWttResult.addTrain(result);
-
-            fetchLocation(trainMovements[i].SchedOriginStanox);
-        }
-    } else {
-        $("#no-results-row").show();
-    }
-}
-
-var currentWttResult = new WttSearchResults();
-
-function getService(wttId) {
-    document.location.hash = "getservice:" + wttId;
-    $.getJSON("http://" + server + ":82/TrainMovement/WithWttId/" + wttId, function (data) {
-        showWttResults(data);
-    });
-}
-
-function getOrigin(args, convertFromCrs) {
-    if (convertFromCrs) {
-        document.location.hash = "listorigin-crs:" + args;
-        $.getJSON("http://" + server + ":82/Stanox/?GetByCrs&crsCode=" + args, function (data) {
-            getOriginByStanox(data.Name);
-        });
-    } else {
-        document.location.hash = "listorigin:" + args;
-        getOriginByStanox(args);
-    }
-}
-
-function getStation(args, convertFromCrs) {
-    if (convertFromCrs) {
-        document.location.hash = "liststation-crs:" + args;
-        $.getJSON("http://" + server + ":82/Stanox/?GetByCrs&crsCode=" + args, function (data) {
-            getCallingAtStanox(data.Name);
-        });
-    } else {
-        document.location.hash = "liststation:" + args;
-        getCallingAtStanox(args);
-    }
-}
-
-function getOriginByStanox(stanox) {
-    listStation(stanox);
-    $.getJSON("http://" + server + ":82/TrainMovement/StartingAtStation/" + stanox, function (data) {
-        showWttResults(data);
-
-        fetchLocation(stanox);
-    });
-}
-
-function getCallingAtStanox(stanox) {
-    listStation(stanox);
-    $.getJSON("http://" + server + ":82/TrainMovement/CallingAtStation/" + stanox, function (data) {
-        showWttResults(data);
-    });
-}
-
-function fetchLocation(stanox) {
-    loadLocation(stanox, function (data) {
-        var html = "";
-        if (data.StationName) {
-            html = data.StationName;
-        } else {
-            html = data.Tiploc;
-        }
-        if (data.CRS) {
-            html += "(" + data.CRS + ")";
-        }
-        $("." + data.Name).html(html);
-        $("." + data.Name).attr('title', data.Description + '(' + data.Name + ')');
-        $("." + data.Name).tooltip();
-        $("." + data.Name).data("title", html);
-    });
-}
-
-function clearData() {
-    currentWttResult.clearTrains();
-}
-
-function preLoadStationsCallback(results) {
-    var commands = [];
-    commands.push('getservice:');
-    commands.push('listorigin:');
-    for (i in results) {
-        commands.push('listorigin:' + results[i].Name)
-    }
-    commands.push('listorigin-crs:');
-    for (i in results) {
-        commands.push('listorigin-crs:' + results[i].CRS)
-    }
-    commands.push('liststation:');
-    for (i in results) {
-        commands.push('liststation:' + results[i].Name)
-    }
-    commands.push('liststation-crs:');
-    for (i in results) {
-        commands.push('liststation-crs:' + results[i].CRS)
-    }
-    $("#filter-command").typeahead({
-        source: commands
-    });
-}
-
-function listStation(stanox) {
-    $('html, body').animate({
-        scrollTop: $("#locationDetails").offset().top
-    }, 1000);
-    $.getJSON("http://" + server + ":82/Stanox/" + stanox, function (data) {
-        currentLocation.locationStanox(data.Name);
-        currentLocation.locationTiploc(data.Tiploc);
-        currentLocation.locationDescription(data.Description);
-        currentLocation.locationCRS(data.CRS);
-        currentLocation.stationName(data.StationName);
-    });
-}
-
-$(function () {
-    preLoadStations(preLoadStationsCallback);
-
-    ko.applyBindings(currentWttResult, $("#train-id-result").get(0));
-
-    ko.applyBindings(currentLocation, $("#stationDetails").get(0));
-
-    loadHashCommand();
-});
-
-function loadHashCommand() {
-    if (document.location.hash.length > 0) {
-        setCommand(document.location.hash.substr(1));
-        parseCommand();
-    }
-    return false;
-}
-
-function setHash(hash) {
-    document.location.hash = hash;
-    loadHashCommand();
-}

WebClient/Scripts/train.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/// <reference path="common.js" />
+﻿/// <reference path="moment.js" />
+/// <reference path="jquery-1.9.1.js" />
 /// <reference path="ViewModels.js" />
 /// <reference path="knockout.mapping-latest.js" />
 /// <reference path="knockout-2.2.1.js" />
@@ -48,7 +48,7 @@ function connectWs() {
         switch (data.Command) {
             case "subtrainupdate":
                 data = data.Response;
-                currentTrain.LastUpdate(formatDateString(new Date()));
+                currentTrain.LastUpdate(moment().format("DD/MM/YY HH:mm:ss"));
                 for (i in data) {
                     addStop(data[i], true);
                     $.when(mapStop(data[i])).then(function () {
@@ -178,7 +178,7 @@ function getTrain(trainId, dontUnSub) {
     if (split && split.length == 2) {
         trainId = split[0] + '/' + split[1];
     }
-    $.getJSON("http://" + server + ":82/TrainMovement/" + trainId, function (data) {
+    $.getJSON("http://" + server + "/TrainMovement/" + trainId, function (data) {
         // if multiple, take first
         if (data.length && data.length > 0)
             data = data[0];
@@ -189,7 +189,7 @@ function getTrain(trainId, dontUnSub) {
         currentTrain.ServiceCode(data.ServiceCode);
         var activated = "";
         if (data.Activated) {
-            activated = formatDateString(new Date(data.Activated));
+            activated = moment(data.Activated).format("DD/MM/YY HH:mm:ss");
         }
         currentTrain.Activated(activated);
         if (data.WorkingTTId && data.WorkingTTId.length > 0) {
@@ -203,10 +203,10 @@ function getTrain(trainId, dontUnSub) {
             fetchLocation(data.SchedOriginStanox);
         var schedDepart = "";
         if (data.SchedOriginDeparture) {
-            schedDepart = formatDateString(new Date(data.SchedOriginDeparture));
+            schedDepart = moment(data.SchedOriginDeparture).format("DD/MM/YY HH:mm:ss");
         }
         currentTrain.SchedDepart(schedDepart);
-        currentTrain.LastUpdate(formatDateString(new Date()));
+        currentTrain.LastUpdate(moment().format("DD/MM/YY HH:mm:ss"));
         showCurrentTrainMap();
         for (i in data.Steps) {
             addStop(data.Steps[i]);
@@ -216,7 +216,7 @@ function getTrain(trainId, dontUnSub) {
         if (data.length && data.length >= 0)
             data = data[0];
 
-        $.getJSON("http://" + server + ":82/Schedule?trainId=" + data.TrainId + "&trainUid=" + data.TrainUid, function (schedule) {
+        $.getJSON("http://" + server + "/Schedule?trainId=" + data.TrainId + "&trainUid=" + data.TrainUid, function (schedule) {
             if (schedule && schedule.Stops) {
                 for (i in schedule.Stops) {
                     schedule.Stops[i].CssClass = "";
@@ -302,7 +302,7 @@ function mapStop(stop) {
         var stanox = stop.Stanox;
         var ts = stop.DepartActualTimeStamp;
     }
-    return $.getJSON("http://" + server + ":82/Stanox/" + stanox, function (data) {
+    return $.getJSON("http://" + server + "/Stanox/" + stanox, function (data) {
         if (data.Lat && data.Lon) {
             marker = new google.maps.Marker({
                 position: new google.maps.LatLng(data.Lat, data.Lon),
@@ -324,7 +324,7 @@ function listStation(stanox) {
         scrollTop: $("#locationDetails").offset().top
     }, 1000);
 
-    $.getJSON("http://" + server + ":82/Stanox/" + stanox, function (data) {
+    $.getJSON("http://" + server + "/Stanox/" + stanox, function (data) {
         currentLocation.locationStanox(data.Name);
         currentLocation.locationTiploc(data.Tiploc);
         currentLocation.locationDescription(data.Description);

WebClient/TrainNotifier.WebClient.csproj
@@ -116,7 +116,6 @@
     <None Include="Scripts\jquery-1.9.1.intellisense.js" />
     <Content Include="Scripts\bootstrap.js" />
     <Content Include="Scripts\bootstrap.min.js" />
-    <Content Include="Scripts\common.js" />
     <Content Include="Scripts\index.js" />
     <Content Include="Scripts\jquery-1.9.1.js" />
     <Content Include="Scripts\jquery-1.9.1.min.js" />
@@ -131,7 +130,6 @@
     <Content Include="Scripts\moment.js" />
     <Content Include="Scripts\moment.min.js" />
     <Content Include="Scripts\search-schedule.js" />
-    <Content Include="Scripts\search.js" />
     <Content Include="Scripts\train.js" />
     <Content Include="Scripts\ViewModels.js" />
     <Content Include="train.cshtml" />

commit db498ab48978d6b576a39948c0fd813ea07c5354

fix view model bugs
introduced in 0e6f6a38c9548b4b81e7f6d0a2d2e9432ecba918

WebClient/Scripts/ViewModels.js
@@ -119,7 +119,8 @@ function getTimes(stopEl) {
         Delay: 0
     };
     if (stopEl.ActualTimeStamp && stopEl.ActualTimeStamp.length > 0) {
-        result.ActualTimeStamp = moment(stopEl.ActualTimeStamp).format("HH:mm:ss");
+        var actualTime = new Date(stopEl.ActualTimeStamp);
+        result.ActualTimeStamp = moment(actualTime).format("HH:mm:ss");
     } else {
         result.ActualTimeStamp = "";
         setTimes = false;
@@ -127,13 +128,14 @@ function getTimes(stopEl) {
 
     if (stopEl.PlannedTime && stopEl.PlannedTime.length > 0) {
         var plannedTime = new Date(stopEl.PlannedTime);
-        result.PlannedTimeStamp = moment(stopEl.PlannedTime).format("HH:mm:ss");
     } else if (stopEl.ActualTimeStamp && stopEl.ActualTimeStamp.length > 0) {
-        result.PlannedTimeStamp = moment(stopEl.ActualTimeStamp).format("HH:mm:ss");
+        var plannedTime = new Date(stopEl.ActualTimeStamp);
     } else {
         result.PlannedTimeStamp = "";
         setTimes = false;
     }
+    if (plannedTime)
+        result.PlannedTimeStamp = moment(plannedTime).format("HH:mm:ss");
 
     if (setTimes) {
         result.Delay = ((actualTime - plannedTime) / 60000);