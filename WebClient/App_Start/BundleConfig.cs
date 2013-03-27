e8a99c5b7dd60139630a639629f2e88a82080b63
Michael Pritchard <blueghostuk@gmail.com>
2013-03-27 21:47:03 +0000

    make bundle config static

--- WebClient/App_Start/BundleConfig.cs
+++ WebClient/App_Start/BundleConfig.cs
@@ -2,7 +2,7 @@
 
 namespace TrainNotifier.WebClient
 {
-    public class BundleConfig
+    public static class BundleConfig
     {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
