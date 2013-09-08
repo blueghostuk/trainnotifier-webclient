dceb219eec65826139a4443390a0cb846a266052
Michael Pritchard <blueghostuk@gmail.com>
2013-09-08 20:52:35 +0100

    #104 update

--- WebClient/Scripts/app/station.js
+++ WebClient/Scripts/app/station.js
@@ -35,163 +35,233 @@ var Station = (function () {
 var platforms = [];
 for (var i = 1; i <= 12; i++) {
     platforms.push(new Platform(i.toString(), [
-        new Section("->" + i + "A"),
+        new Section(i + "A"),
+        new Section(i + "A"),
         new Section(i + "A"),
         new Section(i + "B"),
-        new Section(i + "B ->")
+        new Section(i + "B"),
+        new Section(i + "B")
     ]));
 }
 
 // XOJ = PROOF HOUSE JUNCTION
 // -> 1A
-platforms[0].sections[0].berth = "BN-0149";
+platforms[0].sections[0].berth = "BN-0154";
+
+// <- 1A
+platforms[0].sections[1].berth = "BN-0149";
 
 // 1A
-platforms[0].sections[1].berth = "BN-0181";
+platforms[0].sections[2].berth = "BN-0205";
 
-// 1B ( XOJ? 0154, 0205 -> 1A/B?)
-platforms[0].sections[2].berth = "BN-0209";
+// 1B
+platforms[0].sections[3].berth = "BN-0209";
 
-// 1B ->
-platforms[0].sections[3].berth = "BN-0244";
+// -> 1B
+platforms[0].sections[4].berth = "BN-0471";
 
-// 2A -> BN 0179,0149,0142,0136,0083(XOZ),0442 -> DUD
+// <- 1B
+platforms[0].sections[5].berth = "BN-0244";
+
 // -> 2A
-platforms[1].sections[0].berth = "BN-0149";
+platforms[1].sections[0].berth = "BN-0154";
+
+// <- 2A
+platforms[1].sections[1].berth = "BN-0149";
 
 // 2A
-platforms[1].sections[1].berth = "BN-0179";
+platforms[1].sections[2].berth = "BN-0199";
 
 // 2B
-platforms[1].sections[2].berth = null;
-
-// 2B ->
 platforms[1].sections[3].berth = "BN-0208";
 
+// -> 2B
+platforms[1].sections[4].berth = "BN-0471";
+
+// <- 2B
+platforms[1].sections[5].berth = "BN-0244";
+
 // -> 3A
-platforms[2].sections[0].berth = null;
+platforms[2].sections[0].berth = "BN-0154";
+
+// <- 3A
+platforms[2].sections[1].berth = "BN-0149";
 
 // 3A
-platforms[2].sections[1].berth = "BN-0198";
+platforms[2].sections[2].berth = "BN-0198";
 
 // 3B
-platforms[2].sections[2].berth = null;
-
-// 3B ->
 platforms[2].sections[3].berth = "BN-0206";
 
+// -> 3B
+platforms[2].sections[4].berth = "BN-0471";
+
+// <- 3B
+platforms[2].sections[5].berth = "BN-0244";
+
 // -> 4A
-platforms[3].sections[0].berth = null;
+platforms[3].sections[0].berth = "BN-0154";
+
+// <- 4A
+platforms[3].sections[1].berth = "BN-0149";
 
 // 4A
-platforms[3].sections[1].berth = "BN-0231";
+platforms[3].sections[2].berth = "BN-0203";
 
-// BN-0203 -> 4B -> 0231, 0471 (Tunnel)
 // 4B
-platforms[3].sections[2].berth = "BN-0204";
+platforms[3].sections[3].berth = "BN-0204";
+
+// -> 4B
+platforms[3].sections[4].berth = "BN-0471";
+
+// <- 4B
+platforms[3].sections[5].berth = "BN-0244";
 
 // 4C
-platforms[3].sections.push(new Section("4C", "BN-0229"));
+//platforms[3].sections[3] = new Section("4C", "BN-0229");
+// -> 5A
+platforms[4].sections[0].berth = "BN-0154";
 
-// BN-0244, 0245
-// -> 5A BN-0136,0135(XOJ?),0438,0440,0446 -> BHI
-platforms[4].sections[0].berth = "BN-0149";
+// <- 5A
+platforms[4].sections[1].berth = "BN-0149";
 
 // 5A
-platforms[4].sections[1].berth = "BN-0201";
+platforms[4].sections[2].berth = "BN-0201";
 
 // 5B
-platforms[4].sections[2].berth = "BN-0202";
+platforms[4].sections[3].berth = "BN-0202";
 
-// 5B ->
-platforms[4].sections[3].berth = "BN-0242";
+// -> 5B
+platforms[4].sections[4].berth = "BN-0452";
+
+// <- 5B
+platforms[4].sections[5].berth = "BN-0244";
 
 // -> 6A
-platforms[5].sections[0].berth = null;
+platforms[5].sections[0].berth = "BN-0154";
+
+// <- 6A
+platforms[5].sections[1].berth = "BN-0149";
 
 // 6A
-platforms[5].sections[1].berth = null;
+platforms[5].sections[2].berth = "BN-0194";
 
 // 6B
-platforms[5].sections[2].berth = null;
+platforms[5].sections[3].berth = "BN-0195";
 
-// 6B ->
-platforms[5].sections[3].berth = null;
+// -> 6B
+platforms[5].sections[4].berth = "BN-0452";
 
-// -> 7A BN-0142(XOJ?),0136(XOJ?),0135
-platforms[6].sections[0].berth = "BN-0149";
+// <- 6B
+platforms[5].sections[5].berth = "BN-0244";
+
+// -> 7A
+platforms[6].sections[0].berth = "BN-0154";
+
+// <- 7A
+platforms[6].sections[1].berth = "BN-0149";
 
 // 7A
-platforms[6].sections[1].berth = "BN-0192";
+platforms[6].sections[2].berth = "BN-0192";
 
 // 7B
-platforms[6].sections[2].berth = "BN-0193";
+platforms[6].sections[3].berth = "BN-0193";
 
-// 7B ->
-platforms[6].sections[3].berth = "BN-0243";
+// -> 7B
+platforms[6].sections[4].berth = "BN-0452";
+
+// <- 7B
+platforms[6].sections[5].berth = "BN-0244";
 
 // -> 8A
 platforms[7].sections[0].berth = "BN-0149";
 
+// <- 8A
+platforms[7].sections[1].berth = "BN-0148";
+
 // 8A
-platforms[7].sections[1].berth = "BN-0167";
+platforms[7].sections[2].berth = "BN-0189";
 
-// 0453 (Tunnel) 0243, 0191 -> 8B
-platforms[7].sections[2].berth = "BN-0191";
+// 8B
+platforms[7].sections[3].berth = "BN-0191";
 
-// 8B -> 0148, 0133, 0132 (XOJ)
-// 8B ->
-platforms[7].sections[3].berth = null;
+// -> 8B
+platforms[7].sections[4].berth = "BN-0452";
+
+// <- 8B
+platforms[7].sections[5].berth = "BN-0244";
 
 // -> 9A
 platforms[8].sections[0].berth = "BN-0151";
 
+// <- 9A
+platforms[8].sections[1].berth = "BN-0148";
+
 // 9A
-platforms[8].sections[1].berth = "BN-0187";
+platforms[8].sections[2].berth = "BN-0187";
 
 // 9B
-platforms[8].sections[2].berth = null;
+platforms[8].sections[3].berth = "BN-0188";
 
-// 9B ->
-platforms[8].sections[3].berth = null;
+// -> 9B
+platforms[8].sections[4].berth = "BN-0452";
+
+// <- 9B
+platforms[8].sections[5].berth = "BN-0244";
 
 // -> 10A
-platforms[9].sections[0].berth = null;
+platforms[9].sections[0].berth = "BN-0151";
+
+// <- 10A
+platforms[9].sections[1].berth = "BN-0148";
 
 // 10A
-platforms[9].sections[1].berth = "BN-0184";
+platforms[9].sections[2].berth = "BN-0184";
 
-// BN-0184 -> 10B
 // 10B
-platforms[9].sections[2].berth = "BN-0215";
+platforms[9].sections[3].berth = "BN-0185";
 
-// 10B -> BN-0452(Tunnel) -> FWY
-// 10B ->
-platforms[9].sections[3].berth = null;
+// -> 10B
+platforms[9].sections[4].berth = "BN-0452";
+
+// <- 10B
+platforms[9].sections[5].berth = "BN-0244";
 
 // -> 11A
-platforms[10].sections[0].berth = "BN-0148";
+platforms[10].sections[0].berth = "BN-0151";
+
+// <- 11A
+platforms[10].sections[1].berth = "BN-0148";
 
 // 11A
-platforms[10].sections[1].berth = "BN-0161";
+platforms[10].sections[2].berth = "BN-0183";
 
 // 11B
-platforms[10].sections[2].berth = "BN-0214";
-
-// 11B ->
 platforms[10].sections[3].berth = "BN-X214";
 
-// -> 12A -> 0133. 0123 (XOJ)
+// -> 11B
+platforms[10].sections[4].berth = "BN-0452";
+
+// <- 11B
+platforms[10].sections[5].berth = "BN-0244";
+
+// -> 12A
 platforms[11].sections[0].berth = "BN-0151";
 
+// <- 12A
+platforms[11].sections[1].berth = "BN-0148";
+
 // 12A
-platforms[11].sections[1].berth = "BN-0182";
+platforms[11].sections[2].berth = "BN-0182";
 
 // 12B
-platforms[11].sections[2].berth = "BN-0212";
+platforms[11].sections[3].berth = "BN-X212";
 
-// 12B ->
-platforms[11].sections[3].berth = null;
+// -> 12B
+platforms[11].sections[4].berth = "BN-0452";
+
+// <- 12B
+platforms[11].sections[5].berth = "BN-0244";
 
 var station = new Station("Birmingham New Street", platforms);
 
