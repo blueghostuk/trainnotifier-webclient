fabad21bd8a510780b7dd1c33af36e0daccbc603
Michael Pritchard <blueghostuk@gmail.com>
2013-09-22 18:08:15 +0100

    ??

--- WebClient/Scripts/app/webApi.js
+++ WebClient/Scripts/app/webApi.js
@@ -1,4 +1,4 @@
-/// <reference path="global.ts" />
+﻿/// <reference path="global.ts" />
 /// <reference path="../typings/jquery/jquery.d.ts" />
 /// <reference path="../typings/moment/moment.d.ts" />
 var TrainNotifier;
